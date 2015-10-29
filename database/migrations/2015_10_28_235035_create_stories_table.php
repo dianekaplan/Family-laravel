c9b123bbeddd4285a843f995cbae372fd952642c
Diane Kaplan <dianekaplan@gmail.com>
2015-10-29 07:22:19 -0400

    updated stories table, moved files here, updated link display partial

--- database/migrations/2015_10_28_235035_create_stories_table.php
+++ database/migrations/2015_10_28_235035_create_stories_table.php
@@ -16,7 +16,7 @@ class CreateStoriesTable extends Migration
             $table->increments('id');
             $table->string('description');
             $table->string('image')->nullable();
-            $table->text('text')->nullable();
+            $table->text('intro')->nullable();
             $table->string('slug')->nullable();
             $table->string('source')->nullable();
             $table->timestamps();
--- resources/views/partials/_story_link.blade.php
+++ resources/views/partials/_story_link.blade.php
@@ -1,9 +1,15 @@
 @unless ($subject->stories->isEmpty())
-    <h5>Special info:</h5>
     <div>
+<!--@TODO: add book icon-->
         @foreach($subject->stories as $story)
-            <a href="http://www.newribbon.com/family/special/{{$story->slug}}.htm" target="_blank">{{$story->description}}</a>:
-            {{$story->text}}...<a href="http://www.newribbon.com/family/special/{{$story->slug}}.htm" target="_blank">Read More</a>
+            <a href="stories/{{$story->slug}}" target="_blank">{{$story->description}}</a>:
+            {{$story->intro}}...<a href="stories/{{$story->slug}}" target="_blank">Read More</a>
         @endforeach
+        <br/>
+
+        {{--@foreach($subject->stories as $story)--}}
+            {{--<a href="http://www.newribbon.com/family/special/{{$story->slug}}.htm" target="_blank">{{$story->description}}</a>:--}}
+            {{--{{$story->text}}...<a href="http://www.newribbon.com/family/special/{{$story->slug}}.htm" target="_blank">Read More</a>--}}
+        {{--@endforeach--}}
     </div>
 @endunless
