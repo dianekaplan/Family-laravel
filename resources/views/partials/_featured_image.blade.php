cae7afbeb934d5a0552f87076fef7626ab1c7f2c
Diane Kaplan <dianekaplan@gmail.com>
2015-11-08 09:08:11 -0500

    updating featured partial with the same workaround

--- resources/views/partials/_featured_image.blade.php
+++ resources/views/partials/_featured_image.blade.php
@@ -2,7 +2,12 @@
     @foreach($featured_image as $image)
         {{--<img src="http://res.cloudinary.com/hnyiprajv/image/upload/{{ $image->std_name  }}" class="img-rounded">--}}
 
-        <?php echo cl_image_tag($image->big_name , array( "cloud_name" => "hnyiprajv", "height" => 200, "class"=>"img-rounded" )); ?>
+        {{--@FIXME: same as image_link- need to figure out why this stopped working--}}
+        <?
+{{--        php echo cl_image_tag($image->big_name , array( "cloud_name" => "hnyiprajv", "height" => 200, "class"=>"img-rounded" )); --}}
+        ?>
+
+        <img src="http://res.cloudinary.com/hnyiprajv/image/upload/{{ $image->big_name  }}" height = "200" class="img-rounded">
 
 
     @endforeach
