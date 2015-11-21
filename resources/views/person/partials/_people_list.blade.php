be11b64450312983808f8926e631102ffcc27187
Diane Kaplan <dianekaplan@gmail.com>
2015-11-21 11:35:57 -0500

    adding irish flag icon

--- resources/views/person/partials/_people_list.blade.php
+++ resources/views/person/partials/_people_list.blade.php
@@ -3,7 +3,7 @@
 
     @foreach ($person_group as $person)
         <li>
-            @include ('person.partials._person_link', ['person' => $person, 'show_flag'=>'Y', 'show_book'=>'Y'])
+            @include ('person.partials._person_link', ['person' => $person, 'show_flag'=>'Y', 'show_book'=>'N'])
         </li>
     @endforeach
 @endif
