a862a54d753969a4860e07be31f508b644a77618
Diane Kaplan <dianekaplan@gmail.com>
2015-11-22 21:50:47 -0500

    a little more in the kemler history

--- resources/views/pages/partials/_kemler_history.blade.php
+++ resources/views/pages/partials/_kemler_history.blade.php
@@ -2,12 +2,14 @@
 
 The Kemler side:
         <ul>
-            <li>Gert's father <a href="{{ action('PeopleController@show', [18]) }}">Louis Kemler</a> (3 generations back) came to the US from <b>Russia</b> with his father
-                <a href="{{ action('PeopleController@show', [17]) }}">Solomon </a><b>around 1902</b>- he was
+            <li>Gert's father <a href="{{ action('PeopleController@show', [18]) }}">Louis Kemler</a> (3 generations back) came to the US
+                from <b>Kiev, Ukraine</b> (still part of the Russian empire at the time) with his father
+                <a href="{{ action('PeopleController@show', [17]) }}">Solomon</a> <b>around 1902</b>- he was
                 13 years old.</li>
         </ul>
 
 
 The Kaplan side:
-        <ul><li>*Need to confirm- Bessie's parents <a href="{{ action('FamilyController@show', [3]) }}">Ida & Barnett Kaplan</a> (four generations back) were probably the generation that came (from <b>Lithuania</b>)</li></ul>
+        <ul><li>Bessie's parents <a href="{{ action('FamilyController@show', [3]) }}">Ida & Barnett Kaplan</a> (four generations back) were  the generation that came (from <b>Lithuania</b>).
+                They started in NYC (where Bessie was born in August of 1888), and had moved to massachusetts by 1893 when her sister Gertrude was born.</li></ul>
 
