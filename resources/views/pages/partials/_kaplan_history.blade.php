998f0c079fdbebf7f340a35cab444d07e9cee06c
Diane Kaplan <dianekaplan@gmail.com>
2015-11-22 21:28:02 -0500

    added to Kobrin history partial

--- resources/views/pages/partials/_kaplan_history.blade.php
+++ resources/views/pages/partials/_kaplan_history.blade.php
@@ -2,16 +2,31 @@
 
 The Kobrin side:
         <ul>
-            <li>Larry's father <a href="{{ action('PeopleController@show', [29]) }}">Kal Kobrin</a> (3 generations back) came to the US from <b>Lithuania</b> in <b>1896</b> at the age of 20.
+            <li>Larry's father <a href="{{ action('PeopleController@show', [29]) }}">Kal Kobrin</a> (3 generations back)
+                <b>came to the US from Lithuania</b> in <b>1896</b> at the age of 20.
                 (And several of his siblings settled in South Africa around the same time).  When he immigrated, his surname was changed from Kobrin to Kaplan. (Kalman's mother
-                <a href="{{ action('PeopleController@show', [311]) }}">>Rochel</a> had come to the US in 1894, but may have gone to South Africa afterward- need to confirm). </li>
+                <a href="{{ action('PeopleController@show', [311]) }}">Rochel</a> had come to the US in 1894, but may have gone to South Africa afterward- need to confirm).
+               </li>
         </ul>
 
 
 The Shapiro side:
-        <ul><li><a href="{{ action('FamilyController@show', [119]) }}">Toba & Max Shapiro</a> (4 generations back) moved from Russia to Johannesburg, South Africa.  Their daughter Annie was born in <b>Lithuania</b>, moved with them to South Africa, and came to the US <b>sometime between 1876 and 1896</b>.
+        <ul><li><a href="{{ action('FamilyController@show', [119]) }}">Toba & Max Shapiro</a> (4 generations back) moved from Russia to Johannesburg, South Africa.  Their daughter Annie was born in <b>Lithuania</b> in 1876,
+                moved with them to <b>South Africa</b>, and came to the <b>US sometime between 1876 and 1896</b>.
                 <a href="{{ action('PeopleController@show', [28]) }}">Annie</a> was
                 <a href="{{ action('PeopleController@show', [30]) }}">Larry</a>'s mother. </li></ul>
 
+Background for both families:
+<ul><li>
+Lithuania had been struggling with  oppression under the Russian empire and famine in 1867-1868, and according to
+<a href="https://en.wikipedia.org/wiki/History_of_Lithuania">wikipedia</a> between 1868 and 1914, approximately 635,000 people
+(almost 20 percent of the population) left Lithuania. Toba & Max Shapiro moved from Russia to Johannesburg, South Africa. Their daughters
+        <a href="{{ action('PeopleController@show', [28]) }}">Annie</a> +
+        <a href="{{ action('PeopleController@show', [336]) }}">Rebecca</a> Shapiro (sisters) married
+        <a href="{{ action('PeopleController@show', [29]) }}">Kalman</a> and
+        <a href="{{ action('PeopleController@show', [316]) }}">Solomon</a> Kobrin (brothers).
+        Annie & Kalman in the US (they were married in Boston June 15, 1896), and Rebecca & Solomon stayed in South Africa.  (Add something about when those relatives immigrated)
+
+    </li></ul>
 
 
