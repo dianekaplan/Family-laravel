3c8e99d77d42e75550cc8c11a4248cd541700978
Diane Kaplan <dianekaplan@gmail.com>
2015-12-01 07:22:06 -0500

    Added to the password reset email

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,3 +1,9 @@
 <!-- resources/views/emails/password.blade.php -->
 <!--got this from here along with auth views: https://github.com/bestmomo/scafold/blob/master/views/app.blade.php -->
-Click here to reset your password: {{ url('password/reset/'.$token) }}
+Click here to reset your password for the family tree: <br/>
+{{ url('password/reset/'.$token) }}
+<br/><br/>
+If you have any questions, reply back and let me know!<br/><br/>
+
+Thanks,<br/>
+Diane
