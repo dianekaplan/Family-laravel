128521caf689520e5df9386d2d2ff55afb5e5765
Diane Kaplan <dianekaplan@gmail.com>
2015-12-03 06:40:09 -0500

    small edit to redo last failed push

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -21,7 +21,8 @@
     Are you related to any of the folks below, or see yourself in this list? Welcome to our family tree website!
     I've spent years gathering info, dates, and pictures, and here is a place to share it all.
     To protect our family this website is password-protected, so if you're related and would like an account,
-    please request it <a href="add it">here</a>.
+    please request it
+    <a href="mailto:dianekaplan@gmail.com?Subject=Family%20tree%20user%20request" target="_top">here</a>.
 
     <br/><br/>
     Thanks!<br/>
