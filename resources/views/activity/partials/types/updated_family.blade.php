a8512407c1db6e74e038f3f0f83f36eff7e10f4e
Diane Kaplan <dianekaplan@gmail.com>
2015-12-08 06:44:29 -0500

    updated wording on activity partials

--- resources/views/activity/partials/types/updated_family.blade.php
+++ resources/views/activity/partials/types/updated_family.blade.php
@@ -1,5 +1,5 @@
 <a href="/people/{{$event->user->person_id}}">{{ $event->user->name }}</a>
-updated info for
+updated
 @include ('family.partials._family_link', ['family' => $event->subject, 'generation' => 'NA'])
 
 {{ $event->created_at->diffForHumans() }}
--- resources/views/activity/partials/types/updated_person.blade.php
+++ resources/views/activity/partials/types/updated_person.blade.php
@@ -1,7 +1,7 @@
 {{--{{ $event->user->name }} updated a person {{ $event->created_at->diffForHumans() }}--}}
 
 <a href="/people/{{$event->user->person_id}}">{{ $event->user->name }}</a>
-updated info for
+updated
 @include ('person.partials._person_link_simple', ['person' => $event->subject])
 
 {{ $event->created_at->diffForHumans() }}
