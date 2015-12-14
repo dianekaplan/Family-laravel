9d9980645101c1d42ccb79d980746c09a8ee7ddc
Diane Kaplan <dianekaplan@gmail.com>
2015-12-13 20:12:26 -0500

    added height back in to image partial

--- resources/views/partials/_image_link.blade.php
+++ resources/views/partials/_image_link.blade.php
@@ -4,11 +4,11 @@
     <a href="/image/{{ $image->id  }}">
 
         @if ($image->little_name)
-            <?php echo cl_image_tag($image->little_name , array( "cloud_name" => "hnyiprajv", "class"=>"img-rounded" ));
+            <?php echo cl_image_tag($image->little_name , array( "cloud_name" => "hnyiprajv", "height"=> "100", "class"=>"img-rounded" ));
             ?>
         {{--<img src="http://res.cloudinary.com/hnyiprajv/image/upload/{{ $image->little_name  }}" height = "100" class="img-rounded">--}}
         @else
-            <?php echo cl_image_tag($image->big_name , array( "cloud_name" => "hnyiprajv", "class"=>"img-rounded" ));
+            <?php echo cl_image_tag($image->big_name , array( "cloud_name" => "hnyiprajv", "height"=> "100", "class"=>"img-rounded" ));
             ?>
                 {{--<img src="http://res.cloudinary.com/hnyiprajv/image/upload/{{ $image->big_name  }}" height = "100" class="img-rounded">--}}
         @endif
