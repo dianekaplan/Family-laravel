ac34202c6287ffdc198d601e18844156df11449e
Diane Kaplan <dianekaplan@gmail.com>
2015-12-29 12:40:39 -0500

    updated history to show connection_notes, updated data for that field

--- resources/views/pages/history.blade.php
+++ resources/views/pages/history.blade.php
@@ -1,11 +1,29 @@
 @extends('default')
 
+<?php
+$access_tally=0;
+        $only_one_side = false;
+if ($user->keem_access)  $access_tally +=1;
+if ($user->husband_access)  $access_tally +=1;
+if ($user->kemler_access)  $access_tally +=1;
+if ($user->kaplan_access)  $access_tally +=1;
+
+        if ($access_tally == 1) $only_one_side = true;
+
+?>
+
+
+
 @section('content')
 
     <h3 align="center">
         Our Family Tree : History
     </h3>
 
+    @if($only_one_side)
+    <b>Your relation to this person:</b><br/>
+    {{$user->connection_notes}}
+    @endif
 
     @if($user->keem_access)
         <div style="float: left; width:100}%;" id="family_section">
