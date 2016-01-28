99b01eb48cb2e4b4ea9948034264f5d3dc4262ef
Diane Kaplan <dianekaplan@gmail.com>
2016-01-27 23:21:53 -0500

    re-fixed note display for norman, videotest

--- resources/views/pages/test.blade.php
+++ resources/views/pages/test.blade.php
@@ -5,7 +5,7 @@
 
         <?php echo cl_video_tag($video->name , array( "cloud_name" => "hnyiprajv", "height"=> "150", "class"=>"img-rounded", "controls"=>"true" ));
         ?>
-        
+
     @endforeach
     <br/>
 
--- resources/views/partials/_notes.blade.php
+++ resources/views/partials/_notes.blade.php
@@ -4,18 +4,20 @@
     {{--@include ('person.partials._person_link', ['person' =>  $note->author])--}}
     {{--Also the partial has stuff I don't need in this case, like default image if no face (anyone making a note has face)--}}
 
-    @if($note->author)
+
         <div class="media">
+            @if($note->author)
             <div class= "media-left">
                 {{--originally used class "media-object", but switched to rounded for corners, and the wrapping still works--}}
                 <img class="img-rounded"  src="/faces/{{  $note->face  }}"/>
             </div>
+            @endif
             <div class = "media-body">
 
                 {{--Only show the name if it's a note about someone else, not for self-notes--}}
                 @if ($note->author_name)
                 <a href="{{ action('PeopleController@show', [$note->author]) }}">{{$note->author_name}}</a>:
-                    @endif
+
                 @else
                     <br/>
                 @endif
