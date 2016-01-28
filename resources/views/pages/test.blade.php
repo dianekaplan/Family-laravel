f66638dfb866de2cd8a3f2b9aec8e7e2502d4575
Diane Kaplan <dianekaplan@gmail.com>
2016-01-27 20:14:01 -0500

    updated videotest

--- resources/views/pages/test.blade.php
+++ resources/views/pages/test.blade.php
@@ -5,21 +5,10 @@
 
         <?php echo cl_video_tag($video->name , array( "cloud_name" => "hnyiprajv", "height"=> "150", "class"=>"img-rounded", "controls"=>"true" ));
         ?>
-
-        {{--<a href= "/stories/{{$story->id}}" target="_blank">--}}
-        {{--<img  src="/icons/book.png" height="50"/>--}}
-        {{--{{$story->description}}</a>:--}}
-        {{--{{$story->intro}}...<a href= "/stories/{{$story->id}}"  target="_blank">Read More</a>--}}
+        
     @endforeach
     <br/>
 
-Same issue exists with sample file:
-    <?php echo cl_video_tag("dog" , array( "cloud_name" => "demo", "width"=> "300", "height"=> "300", "controls"=>"true" ));
-    ?>
-    {{--cl_video_tag("dog", :width => 300, :height => 300,--}}
-    {{--:crop => :pad, :background => "blue",--}}
-    {{--:preload => "none", :controls => true,--}}
-    {{--:fallback_content => "Your browser does not support HTML5 video tags" )--}}
 
 </div>
 
