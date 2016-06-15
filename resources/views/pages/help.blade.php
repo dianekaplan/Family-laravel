4f5eaaace462da0fcb5d4631c7e0c014b6fbf920
Diane Kaplan <dianekaplan@gmail.com>
2016-06-14 20:01:01 -0400

    updated the FAQ page

--- resources/views/pages/help.blade.php
+++ resources/views/pages/help.blade.php
@@ -10,11 +10,11 @@
     <h4>What can I do here? </h4>
 
 <ul>
-    <li>Start with the Home link to explore your family history, album, and more. </li>
-    <li>Check out your page and fill in what's missing- everyone's updates go a long way. </li>
-    <li>Share memories onto your relatives' pages, and send me pictures to add so everyone can be included</li>
-    <li>From a person or family page, you can travel up and down through generations using the navigation arrows & person links.</li>
-    <li>To jump to someone specific, use the navbar links: Families, People, or "Me!"</li>
+    <li>The 'Home' link presents a few ways to explore our family history, album, videos, and more. </li>
+    <li>Check your page (linked on the homepage and picture in the top nav) and fill in what's missing- everyone's updates go a long way </li>
+    <li>From a person or family page, travel up and down through generations using the navigation arrows & person links.</li>
+    <li>Share memories onto your relatives' pages, and send any pictures you'd like added</li>
+    <li>To go to someone specific, use the navbar links for indexes: Families, People</li>
     <li><b>Be sure to change your password on 'My Account' </b>(and you can see your activity there too).   </li>
 </ul>
 
@@ -27,23 +27,21 @@
     <h4>Who can see this family tree? </h4>
 
     <ul>
-        <li>Family info is extremely sensitive, so it's a fundamental concern with any family tree.
-            The public websites handle it by having everything private/hidden for people still alive, unless you explicitly
-            grant permission to share your tree with somebody.  Similarly for the site I'm making, the only people able to see any
-            info are known family members who are on the tree already, where I've explicitly created an account for that person.
+        <li>Family info is (of course) extremely sensitive, so privacy is a fundamental concern.
+            The public ancestry websites handle it by hiding info for people who are still alive, unless you explicitly
+            grant permission to share your tree with each other.  Similarly here, the only people able to see any
+            info are known family members who are on the tree themselves, where I've explicitly created an account for that person.
         </li>
 
-        <li> Further, each account is created based on how that person is related, so you're only shown people from the side of the family
-            we have in common.  That way what you see is more relevant, and also it doesn't cast the net any wider than needed.</li>
+        <li> Each account is created based on how that person is related, so you're only shown people from the side of the family
+            we have in common.  That way you only see what's relevant, and the net isn't cast any wider than needed.</li>
 
-            <li>Then once the website is live (for now I'm still building) the only part anyone could potentially find in a search
-            engine will be the <a href="landing">login page</a>. That page does have names and thumbnails on it, so that a potential
-            relative researching an ancestor will be able to find it and email me.  On the previous version of this site I was contacted a
-                couple times by relatives who searched ancestor's names and found that page
-                (it's rare since geneology is a pretty narrow interest). When that happens, I make the person answer
-                some questions about our ancestors to prove they're really 'them' (since we haven't met).  This isn't
-                bulletproof if a potential identity thief took the time to research our ancestors, but it's a much higher
-                barrier to fraud than a public website like Facebook.
+            <li>The only info visible without a login (if this site is returned by a search engine) is the <a href="landing">login page</a>.
+                Names and thumbnail images appear there, so a potential relative researching an ancestor can find it and contact
+                me. (This happens less than once a year, since geneology is a pretty narrow interest). When
+                that happens, I make the person answer some questions about our ancestors to prove they're 'them'
+                (since we haven't met).  This isn't bulletproof if a potential identity thief took the time to research
+                our family ancestors, but it's a much higher barrier to fraud than a public website such as Facebook.
         </li>
 
     </ul>
