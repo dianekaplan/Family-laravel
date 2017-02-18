f464b07e7b2f23c368bd583dd351663bccb32c7b
Diane Kaplan <dianekaplan@gmail.com>
2017-02-18 08:22:18 -0500

    email updates

--- config/mail.php
+++ config/mail.php
@@ -15,9 +15,8 @@ return [
     |
     */
 
-////    'driver' => env('MAIL_DRIVER'),
-//    'driver' => env('MAIL_DRIVER', 'smtp'),
-    'driver' => env('MAIL_DRIVER', 'mailgun'),
+    'driver' => env('MAIL_DRIVER'),
+
 
     /*
     |--------------------------------------------------------------------------
